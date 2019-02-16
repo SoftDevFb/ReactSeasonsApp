3e2b6b8f3b9491ff275d59a1803e1de2355dc0d4
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2019-02-16 16:35:16 -0600

    Start of Section 5 Modern React and Redux

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import SeasonDisplay from './SeasonDisplay';
 
 //Class Based Component
 class App extends React.Component {
+    
+    constructor(props) {                //See notes.txt for constructor function info
+        super(props);                   //See notes.txt for super(props) info
+
+        this.state = { lat: null };
+    }
+
+    //Required render method for every component created in React!
     render() {
         //Gets user location using built in API
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
-        
+
         return <div>Latitude: </div>
     }
 }
