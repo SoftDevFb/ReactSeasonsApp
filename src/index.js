2307dfb7cfc500d5349a1bd7aa7f6be6a50c8400
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2019-02-16 20:45:58 -0600

    Section 5 Completed

--- src/index.js
+++ src/index.js
@@ -6,21 +6,37 @@ import SeasonDisplay from './SeasonDisplay';
 //Class Based Component
 class App extends React.Component {
     
-    constructor(props) {                //See notes.txt for constructor function info
-        super(props);                   //See notes.txt for super(props) info
-
-        this.state = { lat: null };
+    constructor(props) {                                        //See notes.txt for constructor function info
+        super(props);                                           //See notes.txt for super(props) info
+        //This is the only time we do direct
+        //assignment to this.state!!!
+        this.state = { lat: null, errorMessage: '' };           //Initializes state
+                                                            
+        window.navigator.geolocation.getCurrentPosition(        //Gets user location using built in API
+            position => {
+                //We called setState here to update our objects state! 
+                this.setState({ lat: position.coords.latitude });
+                //We do not use:
+                // this.state.lat = position.coords.latitude
+            } ,
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
 
     //Required render method for every component created in React!
     render() {
-        //Gets user location using built in API
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+        //Following block of code shows condition rendering
+      if(this.state.errorMessage && !this.state.lat) {
+          return <div>Error: {this.state.errorMessage}</div>
+      }
 
-        return <div>Latitude: </div>
+      if(!this.state.errorMessage && this.state.lat) {
+          return <div>Latitude: {this.state.lat}</div>
+      }
+
+      return <div>Loading!</div>
     }
 }
 
